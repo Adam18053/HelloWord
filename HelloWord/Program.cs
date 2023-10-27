cac58a9c5d48e6cd7cb7ed68b24dc9f1a1466a9c
indom <indom@AKROPOLIS>
2023-10-27 20:42:56 +0200

    Add project files.

---
+++ HelloWord/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+class Program
+    {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Nazywam się Adam Latko");
+        Console.WriteLine("Witam Wszystkich");
+    }
+    }
+}
+
